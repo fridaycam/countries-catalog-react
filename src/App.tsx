be42168c155483097e21affa4d8068a8f8d6ccd5
Vongsok <dara.vongsok@gmail.com>
2024-09-05 11:06:32 +0700

    Install material ui and axios libraries and also write few lines of code to test in the app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Alert, Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome to Countries Catalog Table</h2>
+      <Button variant='contained' onClick={() => alert('You clicked me. Hello')}>Test Button</Button>
     </div>
   );
 }
